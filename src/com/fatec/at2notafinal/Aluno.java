fca7353721f3ad2f0fe580223bc2902e652ce904
Kaio Sumikawa <kaiosumikawa@gmail.com>
2025-11-17 18:50:26 +0900

    atividade

--- src/com/fatec/at2notafinal/Aluno.java
+++ src/com/fatec/at2notafinal/Aluno.java
@@ -1,4 +1,35 @@
 package com.fatec.at2notafinal;
 
 public class Aluno {
+    private String nome;
+    private float[] notas;
+    private float media;
+    private String status;
+    
+    public Aluno(String nome, float[] notas) {
+        this.nome = nome;
+        this.notas = notas;
+    }
+    
+    public void calcularMedia() {
+        float soma = 0;
+        for (float nota : notas) {
+            soma += nota;
+        }
+        this.media = soma / notas.length;
+    }
+    
+    public void avaliarStatus() {
+        if (media < 6.0) {
+            this.status = "Reprovado";
+        } else if (media <= 9.0) {
+            this.status = "Aprovado";
+        } else {
+            this.status = "Ótimo Aproveitamento";
+        }
+    }
+    
+    public String getNome() { return nome; }
+    public float getMedia() { return media; }
+    public String getStatus() { return status; }
 }
--- src/com/fatec/at2notafinal/Main.java
+++ src/com/fatec/at2notafinal/Main.java
@@ -1,4 +1,28 @@
 package com.fatec.at2notafinal;
 
+import javax.swing.JOptionPane;
+
 public class Main {
+    public static void main(String[] args) {
+        String nome = JOptionPane.showInputDialog("Digite o nome completo do aluno:");
+        
+        float[] notas = new float[3];
+        String[] tiposNotas = {"TP1", "TP2", "TP3"};
+        
+        for (int i = 0; i < 3; i++) {
+            String notaStr = JOptionPane.showInputDialog("Digite a nota " + tiposNotas[i] + ":");
+            notas[i] = Float.parseFloat(notaStr);
+        }
+        
+        Aluno aluno = new Aluno(nome, notas);
+        aluno.calcularMedia();
+        aluno.avaliarStatus();
+        
+        JOptionPane.showMessageDialog(null, 
+            "Aluno: " + aluno.getNome() + "\n" +
+            "Notas: TP1=" + notas[0] + ", TP2=" + notas[1] + ", TP3=" + notas[2] + "\n" +
+            "Média: " + String.format("%.2f", aluno.getMedia()) + "\n" +
+            "Status: " + aluno.getStatus()
+        );
+    }
 }
